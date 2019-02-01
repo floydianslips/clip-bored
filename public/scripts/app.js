d95c18f419e9c35b85c030ab842c43cdc6d49822
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-01 04:10:19 +0000

    adds rough poll-box styles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(() => {
     url: "/api/users"
   }).done((users) => {
     for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      // $("<div>").text(user.name).appendTo($("body"));
     }
   });
 });
