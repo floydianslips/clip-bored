d3db01bbcc4a1adcf1c55eaa1924d72670166859
joshua <josh@joshuadennis.ca>
2019-02-02 03:31:41 +0000

    prior to merging front end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,14 +7,6 @@ $(() => {
   }).done((options) => {
     alert(document.URL);
     for (let i=0; i < options.length; i++) {
-      
-      // let allOptionsForPoll = {};
-      // allOptionsForPoll["option" + i] = {};
-      // allOptionsForPoll["option" + i]['id'] = options[i]['id'];
-      // allOptionsForPoll["option" + i]['polls_id'] = options[i]['polls_id'];
-      // allOptionsForPoll["option" + i]['description'] = options[i]['description'];
-      // allOptionsForPoll["option" + i]['title'] = options[i]['title'];
-      // allOptionsForPoll["option" + i]['points'] = options[i]['points'];
       $("<div>").text(options[i]['title']).appendTo($("body"));
       $("<div>").text(options[i]['description']).appendTo($("body"));
     }
