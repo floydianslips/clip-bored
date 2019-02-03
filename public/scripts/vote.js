dc73d645b150fab4707584d46035a34f002c30c3
SammyLiretteCowen <sammylirettecowen@gmail.com>
2019-02-03 20:02:53 +0000

    Backing up before merging origin with results.js

--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -2,22 +2,35 @@ $(() => {
 const url = window.location.href;
 const result = /[^/]*$/.exec(url)[0];
 let currentTitle = "";
-  
+
   $.ajax({
     method: "GET",
     url: `/api/polls/${result}`
   }).done((options) => {
-     
-     console.log(currentTitle)
-     for(option of options) {
+    let howManyCards = 0;
+    console.log(options[56]);
+      //Loop That Creates 
+     for(key of options) {
+      if (key.polls_id == result) {
       
-      if (option.polls_id == result) {
-      $("<div>").text(option.question).appendTo($("body"));
-      $("<div>").text(option.description).appendTo($("body"));
-      currentTitle = option.title
-      }
+      $("<div>").text(key.question).appendTo($("body"));
+      $("<div>").text(key.description).appendTo($("body"));
+      currentTitle = key.title;
+      howManyCards++;
 
+
+
+
+
+
+
+
+
+      }
+      //Fill Up Forms With Number For Ranking
+      $(".maker-question").text(currentTitle);
     }
     $("<header>").text(currentTitle).appendTo($("body"));
+
   });
 });
