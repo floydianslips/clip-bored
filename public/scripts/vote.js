096eb05a89d3411f6a2b1a9dc5bfe60316a24ca6
SammyLiretteCowen <sammylirettecowen@gmail.com>
2019-02-03 22:32:24 +0000

    Finished rendering of vote page with dropdown menus

--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -2,35 +2,42 @@ $(() => {
 const url = window.location.href;
 const result = /[^/]*$/.exec(url)[0];
 let currentTitle = "";
+let currentCardInPoll = 1;
+let locationOfVoteCards = $('.question-options-answers');
 
+
+function buildVoteCard(data) {
+	let $optionWrapper = $("<div class='option-answer-wrapper'>");
+	let $voteCardAnswerTitle = $("<h3 class='answer answer" + currentCardInPoll + "'>").text(data.question);
+	let $voteCardAnswerDescription = $("<p class='answer-description answer" + currentCardInPoll + "'>").text(data.description);
+	let $dropDown = $("<form class='vote-form vote-dropdown" + currentCardInPoll + "' name='dropdown" + currentCardInPoll + "'><label>Rank me: </label><select class='vote-list" + currentCardInPoll + "'></select></form>");
+	let $fullVoteCard = $optionWrapper.append($voteCardAnswerTitle).append($voteCardAnswerDescription).append($dropDown);
+	return $fullVoteCard;
+}
+
+function loadAllVoteCards() {
   $.ajax({
     method: "GET",
     url: `/api/polls/${result}`
   }).done((options) => {
-    let howManyCards = 0;
-    console.log(options[56]);
-      //Loop That Creates 
-     for(key of options) {
-      if (key.polls_id == result) {
-      
-      $("<div>").text(key.question).appendTo($("body"));
-      $("<div>").text(key.description).appendTo($("body"));
-      currentTitle = key.title;
-      howManyCards++;
-
-
-
-
-
-
-
-
-
-      }
-      //Fill Up Forms With Number For Ranking
-      $(".maker-question").text(currentTitle);
-    }
-    $("<header>").text(currentTitle).appendTo($("body"));
-
-  });
+      //Loop That Creates the Cards
+    for(key of options) {
+			if (key.polls_id == result) {
+			currentTitle = key.title;
+			const newVoteCard = buildVoteCard(key);
+			locationOfVoteCards.append(newVoteCard);
+			currentCardInPoll++;
+			}
+			$(".maker-question").text(currentTitle);
+		}
+		//Loop That Inserts The Value Options Into Each Vote Card
+		$('select').each(function(index) {
+			for (let i = 1; i < currentCardInPoll; i++) {
+				$(this).append("<option value = '" + i + "'>" + i + "</option>");
+			}
+		});
+	})
+};
+//Initates All God
+loadAllVoteCards();
 });
